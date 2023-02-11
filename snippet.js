353df3101cda98b26524f8f746e6992a345932d6
JoseRobertoGonzalez <92043837+JoseRobertoGonzalez@users.noreply.github.com>
2023-02-11 11:31:05 -0600

    Update snippet.js

--- snippet.js
+++ snippet.js
@@ -1,1 +1,11 @@
 
+  useEffect(() => {
+    // fetch('https://joserobertogonzalez.github.io/media-repo/mediaList.json')
+    try {
+      fetch("https://cemedialist-default-rtdb.firebaseio.com/")
+        .then((response) => response.json())
+        .then((data) => setMedia(data));
+    } catch (e) {
+      setError("Invalid video URL entered");
+    }
+  }, []);
